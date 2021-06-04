b060076c64ddaa69865a63c41020abe8a616405b
Ethan Soo Hon <ethanny2@gmail.com>
2021-06-04 14:01:33 -0400

    Initial commit with all my personal information and projects filled in. Used gifs instead of images which means I had to alter the template to work with gifs where querying through Gatsbys GQL

--- src/components/Image/ProjectImg.jsx
+++ src/components/Image/ProjectImg.jsx
@@ -3,7 +3,36 @@ import { StaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 import photogram from '../../images/photogram.gif';
+import elseif from '../../images/elseif.gif';
+import fansite from '../../images/fansite.gif';
+import gamerandomizer from '../../images/gamerandomizer.gif';
+import threejsboilerplate from '../../images/threejsboilerplate.gif';
+import scrabble from '../../images/scrabble.gif';
+import streetwares from '../../images/streetwares.gif';
+
+import placeholder from '../../images/project.jpg';
 import styles from './ProjectImg.module.css';
+
+function chooseGif(filename) {
+  switch (filename) {
+    case 'photogram':
+      return { photo: photogram, alt: 'Photogram dashboard demo' };
+    case 'elseif':
+      return { photo: elseif, alt: 'Photogram dashboard demo' };
+    case 'fansite':
+      return { photo: fansite, alt: 'Photogram dashboard demo' };
+    case 'gamerandomizer':
+      return { photo: gamerandomizer, alt: 'Photogram dashboard demo' };
+    case 'threejsboilerplate':
+      return { photo: threejsboilerplate, alt: 'Photogram dashboard demo' };
+    case 'scrabble':
+      return { photo: scrabble, alt: 'Photogram dashboard demo' };
+    case 'streetwares':
+      return { photo: streetwares, alt: 'Photogram dashboard demo' };
+    default:
+      return placeholder;
+  }
+}
 
 const ProjectImg = ({ filename, alt }) => (
   <StaticQuery
@@ -14,6 +43,7 @@ const ProjectImg = ({ filename, alt }) => (
             node {
               relativePath
               name
+              extension
               childImageSharp {
                 fluid(maxWidth: 1366) {
                   ...GatsbyImageSharpFluid
@@ -26,13 +56,13 @@ const ProjectImg = ({ filename, alt }) => (
     `}
     render={(data) => {
       const image = data.images.edges.find((n) => n.node.relativePath.includes(filename));
-
       if (!image) return null;
+      if (image.node.extension.includes('gif')) {
+        const sourceGif = chooseGif(image.node.name);
+        return <img className={styles.gif} src={sourceGif.photo} alt={sourceGif.alt} />;
+      }
 
-      if (image.node.name.includes('.gif') && image.node.name.includes('photogram'))
-        return <img className={styles.gif} src={photogram} alt="Photogram dashboard demo" />;
       const imageFluid = image.node.childImageSharp?.fluid;
-
       return <Img alt={alt} fluid={imageFluid} />;
     }}
   />
--- src/mock/data.js
+++ src/mock/data.js
@@ -41,29 +41,72 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'streetwares.gif',
+    title: '100 Streetwares',
+    info: 'Ecommerce streetwear store made using Next.js, TypeScript, Tailwind CSS, Framer motion and headless Shopify via the storefront API.',
+    info2:
+      'The goal of this project was to gain insight on how to connect an ecommerce storefront with a CMS backend that could easily be handed off to a client. I eventually chose Next.js for its strong ecommerce facing templates and ended up using headless Shopify as the backend (as opposed to Sanity CMS or Bigcommerce).',
+    url: 'https://100streetwares.vercel.app/',
+    repo: 'https://github.com/ethanny2/streetwares-v2', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'gamerandomizer.gif',
+    title: 'Video Game Randomizer',
+    info: 'Video game site made using Webpack 5, SCSS, and the LAMP stack. REST API made with PHP Silex. The concept for this site was to give video game players/fans the ability to programatically search/filter through a comprehensive game database.',
+    info2:
+      'The mysql database powering the site was built by compiling information through multiple video game data APIs (Giant Bomb , IGBD) and web scraping (How long to beat, MetaCritic). In addition, unlike similar sites/ APIs this includes nearly all released video games from around 1980 - 2015.',
+    url: 'https://game-randomizer.netlify.app/',
+    repo: 'https://github.com/ethanny2/video-game-randomizer', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'threejsboilerplate.gif',
+    title: 'Threejs Webpack Boilerplate',
+    info: 'A starter repo for using Threejs with webpack 4 featuring extensive documentation. Configured for best practices such as code splitting, cache bursting, 3D model draco optimization and more.',
+    info2:
+      'This project was created to provide a good starting point for learners who are new to webpack, JavaScript bundlers, and three.js. I wanted to create an optimal development and production environment that was simple to understand and hassle-free to set up for experts and novices alike.',
+    url: 'https://threejs-webpack-boilerplate.netlify.app/',
+    repo: 'https://github.com/ethanny2/threejs-es6-webpack-barebones-boilerplate', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'elseif.gif',
+    title: 'Else If Clothing',
+    info: 'An ecommerce store made entirely client side with Stripe checkout, webpack 5 optimizations, SCSS and Threejs.',
+    info2:
+      'The original concept for the site was to sell clothing for my short-lived clothing brand "else-if" clothing. The site was originally hosted on Big Cartel and was made using their custom platform which abstracted the product backend and gave developers a nice interface to interact with the backend through templating and ruby + Coffeescript.',
+    url: 'https://elseifclothing.netlify.app/',
+    repo: 'https://github.com/ethanny2/cyberpunk-ecommerce-frontend', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'fansite.gif',
+    title: 'Fansite for Artist (Unofficial Playboi Carti)',
+    info: 'An informational fansite made with PHP scraping, Soundcloud & Youtube iframes, Bootstrap, SCSS, Masonry.js , and webpack 5 for optimizations.',
+    info2: `The original concept for the site, as instructed by a client, was to make a site for them to promote their music. Later that agreement fell through but since I had already made the site I just replaced the subject matter with a trending artist at the time.
+            This site is meant to have an old-school grungy aesthetic with the use of a non-traditional background , animated transparent gifs and other florishes. Similar to the website style of the popular fasion brand AWGE.`,
+    url: 'https://unofficial-playboicarti.netlify.app/',
+    repo: 'https://github.com/ethanny2/retro-fansite-update', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'scrabble.gif',
+    title: 'JAM Stack Scrabble Lite Game',
+    info: 'A JAM Stack application made using Netlify serverless functions, SCSS + CSS custom properties, and webpack 4.',
+    info2: `The original concept for this app was created for the November 2020 Thanksgiving week Codepen Challenge; the task was to create a Codepen celebrating the game of Scrabble.
+    To challenge myself and gain a deeper understanding of CSS Custom Properties and JavaScript animation events I decided create this app using 0 dependencies or animation libraries.
+    Since the "back-end" portion of the application is just a single route/function verifying the word is real I opted to use Netlify serverless functions to make it a JAM stack application.`,
+    url: 'https://word-scramble.netlify.app/',
+    repo: 'https://github.com/ethanny2/codePenScrabbleChallenge', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Want to work with me?',
+  btn: `Let's chat`,
+  email: 'ethanny2@gmail.com',
 };
 
 // FOOTER DATA
@@ -72,27 +115,27 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/ArrayLikeObj',
     },
     {
       id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/ethanny2',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/ethansoohon/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/ethanny2',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
