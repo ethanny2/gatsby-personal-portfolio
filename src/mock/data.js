7e32e4ddfff63412e1cfc63f680f469b1ec47ebb
Ethan Soo Hon <ethanny2@gmail.com>
2021-06-12 18:55:15 -0400

    Fixed some typos in the description changed , and updated my resume link

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,11 +19,11 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile-pic2.png',
-  paragraphOne: `My name is Ethan Soo Hon a software engineer with a Bachelor's in Computer Science from The Thomas J. Watson School of Engineering🎓. 
+  paragraphOne: `My name is Ethan Soo Hon, a software engineer with a Bachelor's in Computer Science from The Thomas J. Watson School of Engineering🎓. 
   I work across the entire stack using JavaScript with a focus primarily on the front end 💻. I love learning new tools/technologies and imparting my knowledge unto others 📚.`,
-  paragraphTwo: `For the past year and a half I have been working as an online tutor guiding numerous college students and over 15 bootcamp students through their web development classes/cirriculum going over topics such
+  paragraphTwo: `For the past year and a half I have been working as an online tutor guiding numerous college students and over 50 boot camp students through their web development classes/curriculum  going over topics such
     as React, Node, Express, authentication , Redux, Apollo, PostgreSQL, PHP and more👨‍🏫.`,
-  paragraphThree: 'Currently seeking remote fullstack / frontend JavaScript opportunities!',
+  paragraphThree: 'Currently seeking remote full stack / frontend JavaScript opportunities!',
   resume: 'https://drive.google.com/file/d/1_Y0HUHPCCRNH4X_JBQaNBB_ChFfoipqS/view?usp=sharing', // if no resume, the button will not show up
 };
 
@@ -57,9 +57,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'gamerandomizer.gif',
     title: 'Video Game Randomizer',
-    info: 'Video game site made using Webpack 5, SCSS, and the LAMP stack. REST API made with PHP Silex. The concept for this site was to give video game players/fans the ability to programatically search/filter through a comprehensive game database.',
+    info: 'Video game site made using Webpack 5, SCSS, and the LAMP stack. REST API made with PHP Silex. The concept for this site was to give video game players/fans the ability to programmatically search/filter through a comprehensive game database.',
     info2:
-      'The mysql database powering the site was built by compiling information through multiple video game data APIs (Giant Bomb , IGBD) and web scraping (How long to beat, MetaCritic). In addition, unlike similar sites/ APIs this includes nearly all released video games from around 1980 - 2015.',
+      'The MySQL database powering the site was built by compiling information through multiple video game data APIs (Giant Bomb , IGBD) and web scraping (How long to beat, MetaCritic). In addition, unlike similar sites/ APIs this includes nearly all released video games from around 1980 - 2015.',
     url: 'https://game-randomizer.netlify.app/',
     repo: 'https://github.com/ethanny2/video-game-randomizer', // if no repo, the button will not show up
     logos: ['javascript', 'sass', 'php', 'mysql', 'jquery', 'webpack', 'xampp', 'heroku'],
@@ -69,9 +69,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'threejsboilerplate.gif',
     title: 'Threejs Webpack Boilerplate',
-    info: 'A starter repo for using Threejs with webpack 4 featuring extensive documentation. Configured for best practices such as code splitting, cache bursting, 3D model draco optimization and more.',
+    info: 'A starter repo for using Threejs with Webpack 4 featuring extensive documentation. Configured for best practices such as code splitting, cache bursting, 3D model draco optimization and more.',
     info2:
-      'This project was created to provide a good starting point for learners who are new to webpack, JavaScript bundlers, and three.js. I wanted to create an optimal development and production environment that was simple to understand and hassle-free to set up for experts and novices alike.',
+      'This project was created to provide a good starting point for learners who are new to Webpack, JavaScript bundlers, and three.js. I wanted to create an optimal development and production environment that was simple to understand and hassle-free to set up for experts and novices alike.',
     url: 'https://threejs-webpack-boilerplate.netlify.app/',
     repo: 'https://github.com/ethanny2/threejs-es6-webpack-barebones-boilerplate', // if no repo, the button will not show up
     logos: ['javascript', 'sass', 'webpack', 'netlify'],
@@ -94,9 +94,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'fansite.gif',
     title: 'Fansite for Artist (Unofficial Playboi Carti)',
-    info: 'An informational fansite made with PHP scraping, Soundcloud & Youtube iframes, Bootstrap, SCSS, Masonry.js , and webpack 5 for optimizations.',
+    info: 'An informational fan site made with PHP scraping, SoundCloud & YouTube iframes, Bootstrap, SCSS, Masonry.js , and Webpack 5 for optimizations.',
     info2: `The original concept for the site, as instructed by a client, was to make a site for them to promote their music. Later that agreement fell through but since I had already made the site I just replaced the subject matter with a trending artist at the time.
-            This site is meant to have an old-school grungy aesthetic with the use of a non-traditional background , animated transparent gifs and other florishes. Similar to the website style of the popular fasion brand AWGE.`,
+            This site is meant to have an old-school grungy aesthetic with the use of a non-traditional background , animated transparent gifs and other flourishes. Similar to the website style of the popular fasion brand AWGE.`,
     url: 'https://unofficial-playboicarti.netlify.app/',
     repo: 'https://github.com/ethanny2/retro-fansite-update', // if no repo, the button will not show up
     logos: ['javascript', 'soundcloud', 'sass', 'php', 'jquery', 'webpack', 'heroku', 'netlify'],
@@ -106,7 +106,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'scrabble.gif',
     title: 'JAM Stack Scrabble Lite Game',
-    info: 'A JAM Stack application made using Netlify serverless functions, SCSS + CSS custom properties, and webpack 4.',
+    info: 'A JAM Stack application made using Netlify serverless functions, SCSS + CSS custom properties, and Webpack 4.',
     info2: `The original concept for this app was created for the November 2020 Thanksgiving week Codepen Challenge; the task was to create a Codepen celebrating the game of Scrabble.
     To challenge myself and gain a deeper understanding of CSS Custom Properties and JavaScript animation events I decided create this app using 0 dependencies or animation libraries.
     Since the "back-end" portion of the application is just a single route/function verifying the word is real I opted to use Netlify serverless functions to make it a JAM stack application.`,
